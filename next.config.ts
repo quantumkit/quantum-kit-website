10f6027e8f7a2184e6b0022ade63bed29a97d1ba
Yohannes90 <mekonnenyohannes90@gmail.com>
2025-05-04 14:42:37 +0300

    QUAN-3 <setting up dependecies and configrations for project>

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,36 @@
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+//   output: 'export',
+//   basePath: process.env.PAGES_BASE_PATH,
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from 'next';
+import { resolve } from 'path';
 
 const nextConfig: NextConfig = {
+  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  webpack: config => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      three: resolve(__dirname, 'node_modules/three'),
+    };
+    return config;
+  },
+  // Optional: You can include these if needed
   output: 'export',
   basePath: process.env.PAGES_BASE_PATH,
 };
